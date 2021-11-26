44c8086133759ec68120da7529bffb0742dc9f71
Finn <32446445+Delafinn@users.noreply.github.com>
2021-11-25 21:45:42 -0700

    Add files via upload

---
+++ characterGenerator.py
@@ -0,0 +1,46 @@
+# Create a random sentence generator with the following format.
+# $name, hair color, age use lists to store these values
+import random
+import sys
+
+namelist = ["alvin", "bob", "robert", "obiwan", "bill", "Morpheus", "lisa", "anne", "aang", "katara", "momo", "fred",]
+haircolor = ["black", "blonde", "white", "red", "blue", "brown", "bald"]
+# Make the app randomly select in the lists for a name, color, age. Populate these lists in your script with random data at least 3 values each
+# now do this randomization 3 times ****
+
+while True:
+    Genquestion = input("Do you want to generate characters?")
+    if Genquestion =="no" or Genquestion == " no":
+        sys.exit("quitting")
+    if Genquestion == "yes" or Genquestion == " yes":
+        name1 = random.choice(namelist)
+        print("First users name is is " + name1)
+        hair1 = random.choice(haircolor)
+        print(name1 + "s hair color is "+ hair1)
+        age1 = random.randint(1,100)
+        print(name1 + "s age is " + str(age1))
+
+        name2 = random.choice(namelist)
+        print("Second users name is is " + name2)
+        hair2 = random.choice(haircolor)
+        print(name2 + "s hair color is "+ hair2)
+        age2 = random.randint(1,100)
+        print(name2 + "s age is " + str(age2))
+
+
+        name3 = random.choice(namelist)
+        print("Third users name is is " + name3)
+        hair3 = random.choice(haircolor)
+        print(name3 + "s hair color is "+ hair3)
+        age3 = random.randint(1,100)
+        print(name3 + "s age is " + str(age3))
+
+
+        if age1 > age2 and age1 > age3:
+            print(name1 + " is older than " + name2 + " and " + name3)
+
+        if age2 > age1 and age2 > age3:
+            print(name2 + " is older than " + name1 + " and " + name3)
+
+        if age3 > age1 and age3 > age2:
+            print(name3 + " is older than " + name1 + " and " + name2)
